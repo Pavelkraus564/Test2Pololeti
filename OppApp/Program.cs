cb90f3e7daaeaa3f97955f96592fc96c23c20772
Pavelkraus <pavelkraus07@gmail.com>
2024-02-29 09:48:50 +0100

    Second

--- OppApp/Program.cs
+++ OppApp/Program.cs
@@ -1,10 +1,77 @@
 ﻿namespace OppApp
 {
-    internal class Program
+    public class BankAccount 
     {
-        static void Main(string[] args)
+         int Deposit;
+       int Withdrawal;
+        public int Balance;
+        public string accountNumber;
+        public int BankCode;
+         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+        } 
+        public class Owner
+        {
+           
+            
+                
+            
+        } 
+
+       public BankAccount(int balance,string accountNumber,int BankCode)  
+        {
+            int BankBalance;
+            string Deposit;
+            string Withdrawal;
+            int amount;
         }
     }
-}
+    internal class Program
+    {
+        
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("vložte peníze ");
+            Console.WriteLine(" napiste kolik chcete uložit penez  ");
+            string Deposit = Console.ReadLine();
+           
+           // int a = SafelyConvertToInt(Deposit);
+
+            Console.WriteLine("kolik chete vybrat");
+            string withdrawal = Console.ReadLine();
+
+           // int b = SafelyConvertToInt(withdrawal);
+          
+           // if 
+
+
+            
+
+        }
+        private int SafelyConvertToInt(int a ,int b )
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+    }
+    
+
+       
+        
+    
+
+
+
+
+
+
+
+} 
+ 
+
+
